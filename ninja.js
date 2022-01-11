ed7a68736978871bc608b3989849efc93f2f8741
samdhein <hein.sam@gmail.com>
2022-01-11 12:07:10 -0800

    initial commit

---
+++ ninja.js
@@ -0,0 +1,26 @@
+class Ninja{
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName(){
+        console.log("Ninja's name is " + this.name)
+    }
+    showStats(){
+        console.log("Ninja's name is " + this.name)
+        console.log("Ninja's strength is " + this.strength)
+        console.log("Ninja's speed is " + this.speed)
+        console.log("Ninja's health is " + this.health)
+    }
+    drinkSake(){
+        this.health += 10;
+        console.log(this.name + "'s health is " + this.health)
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();

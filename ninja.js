8c0be777cfdb6a5d594113e7c57a80fdafcb484d
samdhein <hein.sam@gmail.com>
2022-01-11 12:08:59 -0800

    return statements

--- ninja.js
+++ ninja.js
@@ -7,16 +7,19 @@ class Ninja{
     }
     sayName(){
         console.log("Ninja's name is " + this.name)
+        return this;
     }
     showStats(){
         console.log("Ninja's name is " + this.name)
         console.log("Ninja's strength is " + this.strength)
         console.log("Ninja's speed is " + this.speed)
         console.log("Ninja's health is " + this.health)
+        return this;
     }
     drinkSake(){
         this.health += 10;
         console.log(this.name + "'s health is " + this.health)
+        return this;
     }
 }
 
